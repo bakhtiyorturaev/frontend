40aaa1ce3802a00d8334848779df7e60f174e8cf
VasliddinAli <vasliddinali@gmail.com>
2025-01-29 00:15:20 +0500

    '...'

--- src/views/JournalRequirments.vue
+++ src/views/JournalRequirments.vue
@@ -5,41 +5,125 @@
                 Jurnal talablari
             </h2>
             <div class="main-basic">
-                <h5>“Молия ва интеллектул технологиялар” электрон журналга мақолаларни тақдим этиш шартлари</h5>
-                <p>1. Мақола биринчи бетининг юқори қисмида муаллиф тўғрисида маълумотлар кўрсатилиши керак (фамилияси, исми, отасининг исми тўлиқ кўрсатилиши, иш жойи ва лавозими, илмий даража, унвони(агар бўлса), электрон почта манзили ва мулоқот учун телефон).</p>
-                <p>Масалан: и.ф.д.,проф. Неъматов Илимдор Нусратович. Тошкент давлат иқтисодиёт университетининг “Банк иши” кафедраси доценти e-mail: ilimdor@mail.ru тел: +998-99-888-77-66</p>
-                <p>2. Илмий мақолалар мавзуси “Молия ва интеллектуал технологиялар” илмий электрон журналининг “Рукнлар-тематика”да келтирилган йўналишларнинг бирортасига монанд келиши керак.</p>
-                <p>3. Мақолалар ўзбек, қорақалпоқ, рус ва инглиз тилларида тақдим этилиши мумкин.</p>
-                <p>4. Мақолаларнинг қисқача аннотацияси (4 -7 қатор) ўзбек, рус ва инглиз тилларида берилади. Унда муаммонинг долзарблиги, ўрганиш усули ва қандай натижа бўлиши мумкинлиги қисқа, лўнда ифодаланиши керак. Сўнгра 11-17 тагача калит сўзлар мос равишда учта тилда берилиши лозим.</p>
-                <p>5. Мақолада кўтарилаётган муаммо ёки камчилик ўрганилган, мавзу назарий-амалий ва услубий жиҳатдан пухта бўлиши керак; кўрсатилаётган манбаларнинг ишончлилиги асосланган бўлиши лозим. Қўйилган масалани бартараф этиш ёхуд кўламини қисқартиришга муаллифлик ёндашуви, олинган асосий натижалар, хулоса ва таклифлар ихчам, тушунарли қилиб баён этилиши зарур</p>
-                <p>6. Мақолада қоида тариқасида қуйидаги кўрсаткичлар алоҳида кўрсатилиб, сўнгра уларинг ҳар бирига мос равишда жавоб ёзилиши лозим. Жумладан:</p>
-                <p><b>- Кириш; Введение; Introduction</b></p>
-                <p><b>- Материаллар ва тадқиқот усуллари; Материалы и методы исследования; Materials and methods of reseach</b></p>
-                <p><b>Натижалар ва уларни муҳокама қилиш; Результаты и их обсуждение; Results and their discussion</b></p>
-                <p><b>Хулоса; Заключение; Conclusion</b></p>
-                <p>7. Қиёслаш учун шу соҳадаги олимлар, мутахассислар фикри, меъёрий хужжатлар, стратегик дастурлар, Президент асарлари, мурожаатномаси ва маърузаларидан иқтибос келтириш ва уларга муаллифлик фикрлар бўлиши керак</p>
-                <p>8. Ҳар бир мақоланинг ранг-баранглигини таъминлаш мақсадида 2 тадан кам бўлмаган 3 йилдан кам бўлмаган маълумотларни қамраб олган жадвал (диаграмма, гистограмма, чизма ёки расм)лар бўлса мақсадга мувофиқ бўлади</p>
-                <p>9. Мақоланинг хулоса қисмида қандай йўллар билан муаммо ёки камчиликни ҳал этиш мумкинлиги ҳақида 2-5 та назарий ёки амалий таклиф-тавсиялар келтириш лозим</p>
-                <p>10. Ўзбек тилидаги ў, ғ, қ, ҳ каби ҳарфлар илмий мақола матнида гаплар таркибида тўлиқ ёзилиши шарт</p>
-                <p>11. Таҳририят мақолани қўшимча равишда тақризга тақдим этиши ва ушбу тақриз натижалари асосида мақолани чоп этиш ёки этмаслиги масаласи бўйича тегишли ижобий ёки салбий қарорга келиши мумкин</p>
-                <p>12. Иқтибос(Сноска)лар мақола ичида ҳар бир иқтибос қўштирноқ ичига олиниб, сўнгра сноска қўйилади. Масалан: “Инвестиция жалб этишнинг самарали воситаларидан бири бўлган давлат-хусусий шериклик механизмларини транспорт, энергетика, йўл, коммунал, тиббиёт, таълим каби соҳаларга кенг татбиқ этиш керак”1.</p>
-                <p>13. Мақола сўнгида бевосита фойдаланган адабиётлар рўйхати кетма-кет келтирилади. Шунингдек, муаллиф мақолага билвосита алоқадор адабиёт ва манбаларни ҳам келтириш мумкин</p>
-                <p>14. Мақола электрон вариантда ва қоғоз шаклида топширилиб муаллиф томонидан имзоланган бўлиши керак. Мақола матни шрифтининг размери 14 ҳажми 1,0 интервалда, Times New Roman шрифтда, 6 бетдан 15 бетгача бўлиши мумкин. Қайд этилган ҳажмдан 20,0 фоиз кўп ёки кам бўлиши ҳам мумкин</p>
-                <p>Муаллифларининг ҳуқуқлари Ўзбекистон Республикасининг 2006 йил 20 июлда ЎРҚ-42-сон билан қабул қилинган “Муаллифлик ҳуқуқи ва турдош ҳуқуқлар тўғрисида”ги Қонун асосида ҳимояланади</p>
-                <p>ДИҚҚАТ! Ўзгалар ижодий маҳсули ўзлаштирган, яъни оргиналлиги 70,0 фоиздан кам бўлган плагиат мақолалар қабул қилинмайди</p>
-                <p>Электрон журналда мақола чоп этиш бўйича янада кўп ва аниқ маълумотларга эга бўлиш учун www.fintech.uz сайтга киринг!</p>
-                <h5>“Молия ва интеллекткал технологиялар” илмий электрон журналда чоп этилиши мумкин мақола “Рукнлар-тематика”си.</h5>
-                <p>1.Молия, солиқ, бюджет, суғурта.</p>
-                <p>2.Банк, кредит, давлат қарзи.</p>
-                <p>3.Инвестиция ва инновация.</p>
-                <p>4.Макро ва микроиқтисодиёт.</p>
-                <p>5.Молиявий менежмент ва маркетинг.</p>
-                <p>6.Иқтисодиётни Рақамлаштириш,  агро ва нано технололгиялар.</p>
-                <p>7.Бухгалтерия ҳисоби ва иқтисодий таҳлил.</p>
-                <p>8.Хорижий мамлакатлар иқтисодиёти.</p>
-                <p>9.Кичик, бизнес ва хусусий тадбиркорлик.</p>
-                <p>10.Қимматли қоғрозлар бозори.</p>
-                <p>«Ўзбекистон – 2030» стратегиясидаги 5 та йўналиш ва 100 та мақсаддан бирини гавдалантириган мақолалар.</p>
+                <h5>“Moliya va intellektual texnologiyalar” elektron jurnalga maqolalarni taqdim etish shartlari.</h5>
+                <p>1. Maqola birinchi betining yuqori qismida muallif to‘g‘risida ma’lumotlar ko‘rsatilishi kerak
+                    (familiyasi, ismi, otasining ismi to‘liq ko‘rsatilishi, ish joyi va lavozimi, ilmiy daraja, unvoni
+                    (agar
+                    bo‘lsa), elektron pochta manzili va muloqot uchun telefon).
+                    Masalan: i.f.d., prof. Ne’matov Ilimdor Nusratovich. Toshkent davlat iqtisodiyot universitetining
+                    “Bank ishi” kafedrasi dotsenti e-mail: ilimdor@mail.ru tel: +998-99-888-77-66
+                </p>
+                <p>2. Ilmiy maqolalar mavzusi “Moliya va intellektual texnologiyalar” ilmiy elektron jurnalining
+                    “Ruknlar-tematika”da keltirilgan yo‘nalishlardan birortasiga mos kelishi kerak.</p>
+                <p>3. Maqolalar o‘zbek, qoraqalpoq, rus va ingliz tillarida taqdim etilishi mumkin.
+                </p>
+                <p>4. Maqolalarning qisqacha annotatsiyasi (4-7 qatordan iborat) o‘zbek, rus va ingliz tillarida
+                    beriladi.
+                    Unda muammoning dolzarbligi, o‘rganish usuli va qanday natija bo‘lishi mumkinligi qisqa, lo‘nda
+                    ifodalanishi kerak. So‘ngra 11-17 tagacha kalit so‘zlar mos ravishda uchta tilda berilishi lozim</p>
+                <p>5. Maqolada ko‘tarilayotgan muammo yoki kamchilik o‘rganilgan, mavzu nazariy-amaliy va uslubiy
+                    jihatdan puxta bo‘lishi kerak; ko‘rsatilayotgan manbalarning ishonchliligi asoslangan bo‘lishi
+                    lozim. Qo‘yilgan masalani bartaraf etish yoki ko‘lamini qisqartirishga mualliflik yondashuvi,
+                    olingan asosiy natijalar, xulosa va takliflar ixcham, tushunarli qilib bayon etilishi zarur</p>
+                <p>6. Maqolada qoidaga ko‘ra quyidagi ko‘rsatkichlar alohida ko‘rsatilishi va so‘ngra ularning har
+                    biriga
+                    mos ravishda javob yozilishi lozim. Jumladan:</p>
+                <p>Kirish; Vvedeniye; Introduction.</p>
+                <p>Materiallar va tadqiqot usullari; Materialy i metody issledovaniya; Materials and methods of
+                    research.</p>
+                <p>Natijalar va ularni muhokama qilish; Rezultaty i ikh obsuzhdeniye; Results and their discussion.</p>
+                <p>Xulosa; Zaklyucheniye; Conclusion.</p>
+                <p>7. Qiyoslash uchun shu sohadagi olimlar, mutaxassislar fikri, me’yoriy hujjatlar, strategik
+                    dasturlar,
+                    Prezident asarlari, murojaatnomasi va ma’ruzalaridan iqtibos keltirish va ularga mualliflik fikrlar
+                    bo‘lishi kerak.</p>
+                <p>8. Har bir maqolaning rang-barangligini ta’minlash maqsadida 2 tadan kam bo‘lmagan va 3 yildan
+                    kam bo‘lmagan ma’lumotlarni qamrab olgan jadval (diagramma, gistogramma, chizma yoki
+                    rasm)lar bo‘lsa, maqsadga muvofiq bo‘ladi.</p>
+                <p>9. Maqolaning xulosa qismida qanday yo‘llar bilan muammo yoki kamchilikni hal etish mumkinligi
+                    haqida 2-5 ta nazariy yoki amaliy taklif-tavsiyalar keltirish lozim.</p>
+                <p>10. O‘zbek tilidagi o‘, g‘, q, h kabi harflar ilmiy maqola matnida gaplar tarkibida to‘liq yozilishi
+                    shart.
+                </p>
+                <p>11. Tahririyat maqolani qo‘shimcha ravishda taqrizga taqdim etishi va ushbu taqriz natijalari asosida
+                    maqolani chop etish yoki etmasligi masalasi bo‘yicha tegishli ijobiy yoki salbiy qarorga kelishi
+                    mumkin.</p>
+                <p>12. Iqtibos (snoska)lar maqola ichida har bir iqtibos qo‘shtirnoq ichiga olinib, so‘ngra snoska
+                    qo‘yiladi.
+                    Masalan: “Investitsiya jalb etishning samarali vositalaridan biri bo‘lgan davlat-xususiy sheriklik
+                    mexanizmlarini transport, energetika, yo‘l, kommunal, tibbiyot, ta’lim kabi sohalarga keng tatbiq
+                    etish kerak”.</p>
+                <p>13. Maqola so‘ngida bevosita foydalanilgan adabiyotlar ro‘yxati ketma-ket keltiriladi. Shuningdek,
+                    muallif maqolaga bilvosita aloqador adabiyot va manbalarni ham keltirish mumkin.</p>
+                <p>14. Maqola elektron variantda va qog‘oz shaklida topshirilib, muallif tomonidan imzolangan bo‘lishi
+                    kerak. Maqola matni shriftining razmeri 14, hajmi 1,0 intervalda, Times New Roman shriftida, 6
+                    betdan 15 betgacha bo‘lishi mumkin. Qayd etilgan hajmdan 20% ko‘p yoki kam bo‘lishi ham
+                    mumkin.
+                </p>
+                <p><b>DIQQAT!</b> O‘zgalarning ijodiy mahsuli o‘zlashtirilgan, ya’ni originalligi 70% dan kam bo‘lgan
+                    plagiat
+                    maqolalar qabul qilinmaydi.</p>
+                <p>Elektron jurnalda maqola chop etish bo‘yicha yanada ko‘p va aniq ma’lumotlarga ega bo‘lish uchun
+                    www.fintech.uz saytga kiring!</p>
+                <h5>“Moliya va intellektual texnologiyalar” ilmiy elektron jurnalda chop etilishi mumkin maqola
+                    “Ruknlar-tematika”si.</h5>
+                <p>1. Moliya, soliq, byudjet, sug‘urta.
+                </p>
+                <p>2. Bank, kredit, davlat qarzi.
+                </p>
+                <p>3. Investitsiya va innovatsiya.
+                </p>
+                <p>4. Makro va mikroiqtisodiyot.
+                </p>
+                <p>5. Moliyaviy menejment va marketing.
+                </p>
+                <p>6. Iqtisodiyotni Raqamlashtirish, agro va nano texnologiyalar.
+                </p>
+                <p>7. Buxgalteriya hisobi va iqtisodiy tahlil.
+                </p>
+                <p>8. Xorijiy mamlakatlar iqtisodiyoti.
+                </p>
+                <p>9. Kichik biznes va xususiy tadbirkorlik.
+                </p>
+                <p>10. Qimmatli qog‘ozlar bozori.
+                </p>
+                <p>11. “O‘zbekiston – 2030” strategiyasidagi 5 ta yo‘nalish va 100 ta maqsaddan birini gavdalantirgan
+                    maqolalar.</p>
+                <!-- <h5>“Молия ва интеллектул технологиялар” электрон журналга мақолаларни тақдим этиш шартлари</h5> -->
+                <!-- <p>1. Мақола биринчи бетининг юқори қисмида муаллиф тўғрисида маълумотлар кўрсатилиши керак (фамилияси, исми, отасининг исми тўлиқ кўрсатилиши, иш жойи ва лавозими, илмий даража, унвони(агар бўлса), электрон почта манзили ва мулоқот учун телефон).</p> -->
+                <!-- <p>Масалан: и.ф.д.,проф. Неъматов Илимдор Нусратович. Тошкент давлат иқтисодиёт университетининг “Банк иши” кафедраси доценти e-mail: ilimdor@mail.ru тел: +998-99-888-77-66</p> -->
+                <!-- <p>2. Илмий мақолалар мавзуси “Молия ва интеллектуал технологиялар” илмий электрон журналининг “Рукнлар-тематика”да келтирилган йўналишларнинг бирортасига монанд келиши керак.</p> -->
+                <!-- <p>3. Мақолалар ўзбек, қорақалпоқ, рус ва инглиз тилларида тақдим этилиши мумкин.</p> -->
+                <!-- <p>4. Мақолаларнинг қисқача аннотацияси (4 -7 қатор) ўзбек, рус ва инглиз тилларида берилади. Унда муаммонинг долзарблиги, ўрганиш усули ва қандай натижа бўлиши мумкинлиги қисқа, лўнда ифодаланиши керак. Сўнгра 11-17 тагача калит сўзлар мос равишда учта тилда берилиши лозим.</p> -->
+                <!-- <p>5. Мақолада кўтарилаётган муаммо ёки камчилик ўрганилган, мавзу назарий-амалий ва услубий жиҳатдан пухта бўлиши керак; кўрсатилаётган манбаларнинг ишончлилиги асосланган бўлиши лозим. Қўйилган масалани бартараф этиш ёхуд кўламини қисқартиришга муаллифлик ёндашуви, олинган асосий натижалар, хулоса ва таклифлар ихчам, тушунарли қилиб баён этилиши зарур</p> -->
+                <!-- <p>6. Мақолада қоида тариқасида қуйидаги кўрсаткичлар алоҳида кўрсатилиб, сўнгра уларинг ҳар бирига мос равишда жавоб ёзилиши лозим. Жумладан:</p> -->
+                <!-- <p><b>- Кириш; Введение; Introduction</b></p> -->
+                <!-- <p><b>- Материаллар ва тадқиқот усуллари; Материалы и методы исследования; Materials and methods of reseach</b></p> -->
+                <!-- <p><b>Натижалар ва уларни муҳокама қилиш; Результаты и их обсуждение; Results and their discussion</b></p> -->
+                <!-- <p><b>Хулоса; Заключение; Conclusion</b></p> -->
+                <!-- <p>7. Қиёслаш учун шу соҳадаги олимлар, мутахассислар фикри, меъёрий хужжатлар, стратегик дастурлар, Президент асарлари, мурожаатномаси ва маърузаларидан иқтибос келтириш ва уларга муаллифлик фикрлар бўлиши керак</p> -->
+                <!-- <p>8. Ҳар бир мақоланинг ранг-баранглигини таъминлаш мақсадида 2 тадан кам бўлмаган 3 йилдан кам бўлмаган маълумотларни қамраб олган жадвал (диаграмма, гистограмма, чизма ёки расм)лар бўлса мақсадга мувофиқ бўлади</p> -->
+                <!-- <p>9. Мақоланинг хулоса қисмида қандай йўллар билан муаммо ёки камчиликни ҳал этиш мумкинлиги ҳақида 2-5 та назарий ёки амалий таклиф-тавсиялар келтириш лозим</p> -->
+                <!-- <p>10. Ўзбек тилидаги ў, ғ, қ, ҳ каби ҳарфлар илмий мақола матнида гаплар таркибида тўлиқ ёзилиши шарт</p> -->
+                <!-- <p>11. Таҳририят мақолани қўшимча равишда тақризга тақдим этиши ва ушбу тақриз натижалари асосида мақолани чоп этиш ёки этмаслиги масаласи бўйича тегишли ижобий ёки салбий қарорга келиши мумкин</p> -->
+                <!-- <p>12. Иқтибос(Сноска)лар мақола ичида ҳар бир иқтибос қўштирноқ ичига олиниб, сўнгра сноска қўйилади. Масалан: “Инвестиция жалб этишнинг самарали воситаларидан бири бўлган давлат-хусусий шериклик механизмларини транспорт, энергетика, йўл, коммунал, тиббиёт, таълим каби соҳаларга кенг татбиқ этиш керак”1.</p> -->
+                <!-- <p>13. Мақола сўнгида бевосита фойдаланган адабиётлар рўйхати кетма-кет келтирилади. Шунингдек, муаллиф мақолага билвосита алоқадор адабиёт ва манбаларни ҳам келтириш мумкин</p> -->
+                <!-- <p>14. Мақола электрон вариантда ва қоғоз шаклида топширилиб муаллиф томонидан имзоланган бўлиши керак. Мақола матни шрифтининг размери 14 ҳажми 1,0 интервалда, Times New Roman шрифтда, 6 бетдан 15 бетгача бўлиши мумкин. Қайд этилган ҳажмдан 20,0 фоиз кўп ёки кам бўлиши ҳам мумкин</p> -->
+                <!-- <p>Муаллифларининг ҳуқуқлари Ўзбекистон Республикасининг 2006 йил 20 июлда ЎРҚ-42-сон билан қабул қилинган “Муаллифлик ҳуқуқи ва турдош ҳуқуқлар тўғрисида”ги Қонун асосида ҳимояланади</p> -->
+                <!-- <p>ДИҚҚАТ! Ўзгалар ижодий маҳсули ўзлаштирган, яъни оргиналлиги 70,0 фоиздан кам бўлган плагиат мақолалар қабул қилинмайди</p> -->
+                <!-- <p>Электрон журналда мақола чоп этиш бўйича янада кўп ва аниқ маълумотларга эга бўлиш учун www.fintech.uz сайтга киринг!</p> -->
+                <!-- <h5>“Молия ва интеллекткал технологиялар” илмий электрон журналда чоп этилиши мумкин мақола “Рукнлар-тематика”си.</h5> -->
+                <!-- <p>1.Молия, солиқ, бюджет, суғурта.</p> -->
+                <!-- <p>2.Банк, кредит, давлат қарзи.</p> -->
+                <!-- <p>3.Инвестиция ва инновация.</p> -->
+                <!-- <p>4.Макро ва микроиқтисодиёт.</p> -->
+                <!-- <p>5.Молиявий менежмент ва маркетинг.</p> -->
+                <!-- <p>6.Иқтисодиётни Рақамлаштириш,  агро ва нано технололгиялар.</p> -->
+                <!-- <p>7.Бухгалтерия ҳисоби ва иқтисодий таҳлил.</p> -->
+                <!-- <p>8.Хорижий мамлакатлар иқтисодиёти.</p> -->
+                <!-- <p>9.Кичик, бизнес ва хусусий тадбиркорлик.</p> -->
+                <!-- <p>10.Қимматли қоғрозлар бозори.</p> -->
+                <!-- <p>«Ўзбекистон – 2030» стратегиясидаги 5 та йўналиш ва 100 та мақсаддан бирини гавдалантириган мақолалар.</p> -->
             </div>
         </div>
     </div>
